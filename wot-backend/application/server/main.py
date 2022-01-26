cf74124557b3758620fc7cc47db56ccc201dad55
Kronox <dev@kronox.me>
2022-01-26 20:34:32 +0100

    improved Docker build & deployment

--- wot-backend/application/server/main.py
+++ wot-backend/application/server/main.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 from starlette.responses import RedirectResponse
 
 from application.components import predict, read_imagefile
@@ -10,7 +11,14 @@ app_desc = """"""
 
 app = FastAPI(title='Sketch Classification API', description=app_desc)
 
-
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/predict/image")
 async def predict_api(file: UploadFile = File(...)):
