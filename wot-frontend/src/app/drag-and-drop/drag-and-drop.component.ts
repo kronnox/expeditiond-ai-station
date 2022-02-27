b6260fc4fc8caa23283f774b9a029e1f01a49bb6
Kronox <dev@kronox.me>
2022-02-27 20:34:06 +0100

    restyled data-labeling

--- wot-frontend/src/app/drag-and-drop/drag-and-drop.component.ts
+++ wot-frontend/src/app/drag-and-drop/drag-and-drop.component.ts
@@ -11,7 +11,7 @@ import { DragImage } from './model/drag-image';
   styleUrls: ['./drag-and-drop.component.scss']
 })
 export class DragAndDropComponent implements AfterViewInit {
-  
+
   @ViewChild('dropZone') public dropZone: ElementRef;
   @ViewChild('dragZone') public dragZone: ElementRef;
 
@@ -24,29 +24,28 @@ export class DragAndDropComponent implements AfterViewInit {
 
   ngAfterViewInit(): void {
     setTimeout(() => {
-      const size = 200;
+      const size = 150;
       const rectZone = this.dragZone.nativeElement.getBoundingClientRect();
       this.data.forEach(element => {
-        const y = Math.random()*(rectZone.height - size) + (rectZone.y);
-        const x = Math.random()*(rectZone.width - size) + (rectZone.x);
+        const y = Math.random() * (rectZone.height - size) + (rectZone.y);
+        const x = Math.random() * (rectZone.width - size) + (rectZone.x);
         this.dragObjects = [...this.dragObjects, new DragImage(element, x, y, size)];
       })
       this.cdRef.detectChanges();
-      console.log(this.dragObjects)
     }, 0);
-    
+
   }
 
   public changePosition(event: any, item: DragImage): void {
 
-    if(event.previousContainer !== event.container) {
+    if (event.previousContainer !== event.container) {
       transferArrayItem(event.previousContainer.data, event.container.data, this.dragObjects.indexOf(item), event.currentIndex);
     } else {
       const rectZone = this.dropZone.nativeElement.getBoundingClientRect();
-      const rectElement=event.item.element.nativeElement.getBoundingClientRect()
-      
-      let y =+ item.y+event.distance.y;
-      let x =+ item.x+event.distance.x;
+      const rectElement = event.item.element.nativeElement.getBoundingClientRect()
+
+      let y =+ item.y + event.distance.y;
+      let x =+ item.x + event.distance.x;
 
       if(y < rectZone.top) {
         y = rectZone.top;
@@ -54,25 +53,25 @@ export class DragAndDropComponent implements AfterViewInit {
       if(x < rectZone.left) {
         x = rectZone.left;
       }
-      if(y > (rectZone.bottom-rectElement.height)) {
-        y = rectZone.bottom-rectElement.height;
+      if(y > (rectZone.bottom - rectElement.height)) {
+        y = rectZone.bottom - rectElement.height;
       }
-      if(x > (rectZone.right-rectElement.width)) {
-        x = rectZone.right-rectElement.width;
+      if(x > (rectZone.right - rectElement.width)) {
+        x = rectZone.right - rectElement.width;
       }
 
-      item.y=y;
-      item.x=x;
+      item.y = y;
+      item.x = x;
     }
   }
 
   public changeZIndex(item: DragImage): void {
       for(let i of this.dragObjects) {
-        (i==item ? i.z = 1 : i.z = 0)
+        (i == item ? i.z = 1 : i.z = 0)
       }
   }
-  
+
   public continue(): void {
-    this.router.navigate(['/data-grouping']);
+    void this.router.navigate(['/data-grouping']);
   }
 }
--- wot-frontend/src/app/drag-and-drop/drag-and-drop.module.ts
+++ wot-frontend/src/app/drag-and-drop/drag-and-drop.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { DragAndDropComponent } from './drag-and-drop.component';
@@ -15,6 +15,7 @@ import { DragAndDropComponent } from './drag-and-drop.component';
   ],
   exports: [
     DragAndDropComponent
-  ]
+  ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class DragAndDropModule { }
