5913445698efe7b39d47d6dba2132291be8fa4a2
Kronox <dev@kronox.me>
2022-03-02 13:53:54 +0100

    style cleanup

--- wot-frontend/src/app/common/layout/layout.module.ts
+++ wot-frontend/src/app/common/layout/layout.module.ts
@@ -1,10 +1,5 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {WotHeaderComponent} from './wot-header/wot-header.component';
-import {WotHeaderTitleComponent} from './wot-header-title/wot-header-title.component';
-import {WotSidebarComponent} from './wot-sidebar/wot-sidebar.component';
-import {WotToolbarComponent} from './wot-toolbar/wot-toolbar.component';
-import { WotFooterComponent } from './wot-footer/wot-footer.component';
 import { WotStepInfoComponent } from './wot-step-info/wot-step-info.component';
 import { WotRowComponent } from './wot-row/wot-row.component';
 import { WotContainerComponent } from './wot-container/wot-container.component';
@@ -15,11 +10,6 @@ import {ButtonsModule} from "../buttons/buttons.module";
 
 @NgModule({
     declarations: [
-        WotHeaderComponent,
-        WotHeaderTitleComponent,
-        WotSidebarComponent,
-        WotToolbarComponent,
-        WotFooterComponent,
         WotStepInfoComponent,
         WotRowComponent,
         WotContainerComponent,
@@ -28,11 +18,6 @@ import {ButtonsModule} from "../buttons/buttons.module";
     ],
     exports: [
         WotContainerComponent,
-        WotHeaderComponent,
-        WotHeaderTitleComponent,
-        WotSidebarComponent,
-        WotToolbarComponent,
-        WotFooterComponent,
         WotRowComponent,
         WotPageComponent,
         WotStepInfoComponent,
